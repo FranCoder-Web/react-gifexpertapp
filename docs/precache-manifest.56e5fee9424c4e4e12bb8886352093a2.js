b195c29953f8a54d60ef38f99c6430f13ae9bca1
FranCoder <francoder.web@gmail.com>
2020-08-06 06:40:27 -0300

    docs agregado

---
+++ docs/precache-manifest.56e5fee9424c4e4e12bb8886352093a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60470bfce528258e7846695018b5f46f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6093985ed2d113ae89b6",
+    "url": "/static/css/main.14d26621.chunk.css"
+  },
+  {
+    "revision": "8e82f2b95622b5165d58",
+    "url": "/static/js/2.a8fc3c47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8fc3c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6093985ed2d113ae89b6",
+    "url": "/static/js/main.00676720.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
